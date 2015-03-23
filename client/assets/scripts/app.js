5cf02ec192159d92d357b5bb4f86667afe81fccc
Ben Halverson <benhalverson33@gmail.com>
2015-03-23 02:32:06 -0700

    added travis.yml

--- client/assets/scripts/app.js
+++ client/assets/scripts/app.js
@@ -1,8 +1,9 @@
+'use strict';
 var app = angular.module('dailyjobsapp' ['angular-route'])
 	.config(function ($routeProvider){
 			$routeProvider
 					.when('/posts', {
 						templateUrl: './public/posts.html',
 						controller: 'PostsCtrl'
-					})
+					});
 		});
